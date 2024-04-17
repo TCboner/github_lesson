1013a915bac1aee58c632c7fcf2885093850b9c3
TCboner carboone <mexicanfalafel@gmail.com>
2024-04-17 22:07:10 +0000

    copy code from p5

--- sketch.js
+++ sketch.js
@@ -1,40 +1,24 @@
+let pt1 =100;
+let pt2 = 300
+let y = 200;
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(400, 400);
+  pt1=createVector(200,200);
+  pt2=createVector(0,200);
+}
+
+function draw() {
+  background(220);
+  fill(255);
+  pt1.x= mouseX;
+  pt1.y= mouseY;
+  ellipse(pt1.x,pt1.y,50,50);
+  fill(255,255,0);
   
-  function draw() {
-    background(220);
-    background("green")
-    strokeWeight(5)
-    fill('white')
-    ellipse(175, 300, 50, 30)
-    ellipse(225, 300, 50, 30)
-    fill('yellow')
-    ellipse(165, 250, 30, 50)
-    ellipse(235, 250, 30, 50)
-    rect(160, 210, 80, 80)
-    
-    
-    
-    triangle(165, 220, 200, 230, 185, 240)
-    triangle(235, 220, 200, 230, 215, 240)
-    fill('white')
-    strokeWeight(3)
-    ellipse(200, 242, 10, 10)
-    strokeWeight(5)
-    ellipse(170, 115, 45, 80)
-    ellipse(230, 115, 45, 80)
-    ellipse(200, 180, 120, 100)
-    strokeWeight(0)
+  print(pt2.x);
   
-    ellipse(175, 140, 35, 25)
-    ellipse(225, 140, 35, 25)
-    fill('black')
-    ellipse(180, 185, 10, 10)
-    ellipse(220, 185, 10, 10)
-    strokeWeight(5)
-    line(195, 200, 205, 205);
-    line(195, 205, 205, 200);
-    
-    
-  }
+  pt2.x=lerp(pt2.x, pt1.x, 0.1);
+  pt2.y=lerp(pt2.y, pt1.y, 0.1);
+  
+  ellipse(pt2.x,pt2.y,50,50);
+}
